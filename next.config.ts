d275053a133dade2b6410b77fb80700df2a73a92
Elşən Camalov <73233045+Elsen001@users.noreply.github.com>
2026-03-18 11:25:35 +0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,16 @@ const nextConfig: NextConfig = {
   i18n: {
     locales: ["az", "ru"], // Dəstəklənən dillər
     defaultLocale: "az" // Standart dil
-  }
+  },
+  async redirects() {
+    return [
+      {
+        source: '/ru',
+        destination: '/',  // ana sayfa
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
